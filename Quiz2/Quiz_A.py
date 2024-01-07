91c4fb83db6dbe700725702c0cb36b6ff6a4b7b9
GyeongJoon <jjangjoon328@naver.com>
2024-01-07 16:46:44 +0900

    update

---
+++ Quiz2/Quiz_A.py
@@ -0,0 +1,4 @@
+from random import *
+words = ["apple", "banana", "orange"]
+word = choice(words)
+print(word)
---
+++ Quiz3/Quiz_A.py
@@ -0,0 +1,3 @@
+for i in range(1,21):
+    if i % 2 == 1: # i 를 2로 나눈 나머지
+        print("A" + str(i), end=" ")
